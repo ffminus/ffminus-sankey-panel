ddf3396d7ed93638ef16a8ee46854f519ebed48b
ffminus <ffminus@protonmail.com>
2024-12-07 22:40:44 +0100

    Let user configure units and decimals

--- src/module.ts
+++ src/module.ts
@@ -1,7 +1,17 @@
-import { PanelPlugin } from '@grafana/data';
+import { FieldConfigProperty, PanelPlugin } from '@grafana/data';
 
 import { Sankey } from 'components/Sankey';
 import { Options } from 'options';
 
+/** Hide all standard options except the ones that apply to our panel. */
+const standardOptionsToInclude = ['unit', 'decimals'];
+
 /** Declare panel plugin, along with its configuration widgets. */
-export const plugin = new PanelPlugin<Options>(Sankey);
+export const plugin = new PanelPlugin<Options>(Sankey)
+  .useFieldConfig({
+    standardOptions: Object.fromEntries(
+      Object.values(FieldConfigProperty)
+        .filter((field) => !standardOptionsToInclude.includes(field))
+        .map((field) => [field, { hideFromDefaults: true }])
+    ),
+  });
