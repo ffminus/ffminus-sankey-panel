fbf095150acd915d16b509c41d988264e4aacb9e
ffminus <ffminus@protonmail.com>
2024-12-07 22:40:47 +0100

    Expose configuration options to panel user

--- src/module.ts
+++ src/module.ts
@@ -1,7 +1,7 @@
-import { FieldConfigProperty, PanelPlugin } from '@grafana/data';
+import { FieldConfigProperty, PanelPlugin, SelectFieldConfigSettings } from '@grafana/data';
 
 import { Sankey } from 'components/Sankey';
-import { Options } from 'options';
+import { Alignment, ALIGNMENTS, Color, COLORS, Options } from 'options';
 
 /** Hide all standard options except the ones that apply to our panel. */
 const standardOptionsToInclude = ['unit', 'decimals'];
@@ -14,4 +14,87 @@ export const plugin = new PanelPlugin<Options>(Sankey)
         .filter((field) => !standardOptionsToInclude.includes(field))
         .map((field) => [field, { hideFromDefaults: true }])
     ),
-  });
+  })
+  .setPanelOptions((builder) =>
+    builder
+      .addRadio<Alignment, SelectFieldConfigSettings<Alignment>>({
+        path: 'alignment',
+        name: 'Alignment',
+        defaultValue: 'Justify',
+        settings: { options: Object.keys(ALIGNMENTS).map((a) => ({ value: a as Alignment, label: a })) },
+      })
+      .addRadio({
+        path: 'node.colors.kind',
+        name: 'Node color type',
+        defaultValue: 'palette',
+        settings: {
+          options: [
+            { value: 'single', label: 'Single' },
+            { value: 'palette', label: 'Palette' },
+          ],
+        },
+      })
+      .addColorPicker({
+        path: 'node.colors.single',
+        name: 'Node color',
+        defaultValue: 'green',
+        showIf: ({ node }) => node.colors.kind === 'single',
+      })
+      .addSelect<Color, SelectFieldConfigSettings<Color>>({
+        path: 'node.colors.palette',
+        name: 'Node color palette',
+        showIf: ({ node }) => node.colors.kind === 'palette',
+        defaultValue: 'Category',
+        settings: { options: Object.keys(COLORS).map((color) => ({ value: color as Color, label: color })) },
+      })
+      .addSliderInput({
+        path: 'node.width',
+        name: 'Node width',
+        description: 'Width of nodes, in pixels',
+        defaultValue: 15,
+        settings: { min: 5, max: 100, step: 1 },
+      })
+      .addSliderInput({
+        path: 'node.padding',
+        name: 'Node padding',
+        description: 'Padding between nodes, in pixels',
+        defaultValue: 20,
+        settings: { min: 0, max: 200, step: 1 },
+      })
+      .addSliderInput({
+        path: 'node.label.size',
+        name: 'Node label font size',
+        description: 'Font size of node label, in pixels',
+        defaultValue: 16,
+        settings: { min: 4, max: 256, step: 1 },
+      })
+      .addColorPicker({
+        path: 'node.label.color',
+        name: 'Node label font color',
+        defaultValue: 'white',
+      })
+      .addRadio({
+        path: 'link.colors.kind',
+        name: 'Link color type',
+        defaultValue: 'gradient',
+        settings: {
+          options: [
+            { value: 'single', label: 'Single' },
+            { value: 'gradient', label: 'Gradient' },
+          ],
+        },
+      })
+      .addColorPicker({
+        path: 'link.colors.single',
+        name: 'Link color',
+        defaultValue: 'grey',
+        showIf: ({ link }) => link.colors.kind === 'single',
+      })
+      .addSliderInput({
+        path: 'link.opacity',
+        name: 'Link opacity',
+        description: 'Opacity of links, from transparent to fully opaque',
+        defaultValue: 0.4,
+        settings: { min: 0.0, max: 1.0, step: 0.01 },
+      })
+  );
--- src/options.ts
+++ src/options.ts
@@ -1,2 +1,76 @@
+import {
+  schemeAccent,
+  schemeBlues,
+  schemeBrBG,
+  schemeCategory10,
+  schemeDark2,
+  schemeGreens,
+  schemeGreys,
+  schemeOranges,
+  schemePaired,
+  schemePastel1,
+  schemePastel2,
+  schemePuBuGn,
+  schemePurples,
+  schemeRdYlBu,
+  schemeRdYlGn,
+  schemeReds,
+  schemeSet1,
+  schemeSet2,
+  schemeSet3,
+  schemeSpectral,
+  schemeTableau10,
+  schemeYlGnBu,
+  schemeYlOrBr,
+  schemeYlOrRd,
+} from 'd3';
+import { sankeyCenter, sankeyJustify, sankeyLeft, sankeyRight } from 'd3-sankey';
+
 /** Configuration parameters parsed from panel options. */
-export interface Options { }
+export interface Options {
+  alignment: Alignment;
+  node: {
+    colors: { kind: 'single' | 'palette'; single: string; palette: Color };
+    width: number;
+    padding: number;
+    label: { size: number; color: string };
+  };
+  link: { colors: { kind: 'single' | 'gradient'; single: string }; opacity: number };
+}
+
+/** Accepted options for node alignment. */
+export type Alignment = keyof typeof ALIGNMENTS;
+
+/** Map a user input string to a specific algorithm. */
+export const ALIGNMENTS = { Justify: sankeyJustify, Left: sankeyLeft, Center: sankeyCenter, Right: sankeyRight };
+
+/** Accepted options for node colors. */
+export type Color = keyof typeof COLORS;
+
+/** Available color schemes for nodes. */
+export const COLORS = {
+  Category: schemeCategory10,
+  Accent: schemeAccent,
+  Blue: schemeBlues[5],
+  BrBG: schemeBrBG[5],
+  Dark: schemeDark2,
+  Green: schemeGreens[5],
+  Grey: schemeGreys[5],
+  Orange: schemeOranges[5],
+  Paired: schemePaired,
+  'Pastel 1': schemePastel1,
+  'Pastel 2': schemePastel2,
+  PuBuGn: schemePuBuGn[5],
+  Purple: schemePurples[5],
+  RdYlBu: schemeRdYlBu[5],
+  RdYlGn: schemeRdYlGn[5],
+  Red: schemeReds[5],
+  'Set 1': schemeSet1,
+  'Set 2': schemeSet2,
+  'Set 3': schemeSet3,
+  Spectral: schemeSpectral[5],
+  Tableau: schemeTableau10,
+  YlGnBu: schemeYlGnBu[5],
+  YlOrBr: schemeYlOrBr[5],
+  YlOrRd: schemeYlOrRd[5],
+};
